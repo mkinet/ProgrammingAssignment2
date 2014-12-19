6bbe65da31dcade4efe3afe17f801e5f2fa3970e
mkinet <mkinet@gmail.com>
2014-12-19 17:25:15 +0100

    Completion of Assignement 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The following function creates an object that can store a matrix
+## and its inverse in the cache.  Four methods exists to manipulate
+## the object : 'set' and 'get' to store and recover the matrix stored
+## in cache; 'setinverse' and 'getinverse' to set the inverse of the
+## matrix and recover it.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(A = matrix()) {
+        inv <- NULL
+        set <- function(y) {
+                A <<- y
+                inv <<- NULL
+        }
+        get <- function() A
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse) 
 }
 
 
-## Write a short comment describing this function
+## The following function is used to recover the inverse of a matrix
+## from the cache. If the inverse has not been computed yet, the
+## methods call 'solve' to compute it and store the result in the
+## cache for subsequent use.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Here, the argument A must be an object created by the
+## makeCacheMatrix()
+
+cacheSolve <- function(A, ...) {
+        ## Return a matrix that is the inverse of 'A'
+        inv <- A$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- A$get()
+        inv <- solve(data, ...)
+        A$setinverse(inv)
+        inv
 }
